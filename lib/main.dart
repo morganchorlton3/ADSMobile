c797cf0a35d910dc461d1033c9286d27d39f738d
morganchorlton3 <morganchorlton3@gmail.com>
2020-04-24 15:06:48 +0100

    added order view

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
 import 'package:ads/models/order.dart';
+import 'package:ads/orderView.dart';
 import 'package:flutter/material.dart';
-//import 'package:http/http.dart' as http;
 import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       
@@ -37,33 +27,17 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-
-
 List<Order> _data = generateItems(8);
 
 class _MyHomePageState extends State<MyHomePage> {
 
+
   MapboxNavigation _directions;
-  //bool _arrived = false;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -113,17 +87,25 @@ Widget _buildPanel() {
                     child: Icon(Icons.directions),
                     onPressed: () async {
 
-                    await _directions.startNavigation(
-                      origin: Location(name: "Current", latitude: 53.49183, longitude: -2.09196),
-                      destination: Location(name: "Name", latitude: order.lat, longitude: order.lng),
-                      mode: NavigationMode.drivingWithTraffic,
-                      simulateRoute: true, language: "English", units: VoiceUnits.metric);
+                    final cityhall = Location(name: "City Hall", latitude: 42.886448, longitude: -78.878372);
+    final downtown = Location(name: "Downtown Buffalo", latitude: 42.8866177, longitude: -78.8814924);
+            
+    await _directions.startNavigation(
+                                origin: cityhall, 
+                                destination: downtown, 
+                                mode: NavigationMode.drivingWithTraffic, 
+                                simulateRoute: false,
+                                language: "French");
 
                     },
                   ),
                   RaisedButton(
                     child: Text("I'm Here"),
                     onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => OrderView()),
+                      );
                   },
                 )
                 ],
---
+++ lib/orderView.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class OrderView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+             leading: IconButton(icon:Icon(Icons.arrow_back),
+              onPressed:() => Navigator.pop(context, false),
+            ),
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            title: Text('ADS Order'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
