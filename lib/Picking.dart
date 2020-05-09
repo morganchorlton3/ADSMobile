fbffdb4d466fa72665086f1c02a874363ed9de22
morganchorlton3 <morganchorlton3@gmail.com>
2020-05-09 22:32:45 +0100

    added picker page and working barcode scanner

--- lib/Picking.dart
+++ lib/Picking.dart
@@ -2,13 +2,15 @@ import 'package:ads/login.dart';
 import 'package:ads/models/PickingProduct.dart';
 import 'package:ads/models/compleatedOrder.dart';
 import 'package:ads/models/order.dart';
+import 'package:ads/services/webService.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'package:rflutter_alert/rflutter_alert.dart';
-import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class PickingPage extends StatefulWidget {
 
@@ -19,57 +21,14 @@ class PickingPage extends StatefulWidget {
 int orderCounter = 0;
 int ordersCount = 0;
 
-List<CompletedOrder> compleatedOrders = new List<CompletedOrder>();
-
-
-incrementCounter(Order order, String signature) async{
-  CompletedOrder completedOrder = new CompletedOrder(order.id, "Hello", 1, order.userID);
-  compleatedOrders.add(completedOrder);
-  for(var i = 0; i < compleatedOrders.length; i++){
-       // print(compleatedOrders[i].toJson());
-  }
-  /*final prefs = await SharedPreferences.getInstance();
-  int orderCounter = prefs.get('orderCounter');
-  if(orderCounter == null){
-    orderCounter = 0;
-  }else{
-    orderCounter++;
-  }
-  prefs.setInt('orderCounter', orderCounter);
-  print(orderCounter);
-  */
-  orderCounter++;  
-}
-
 class _PickingPageState extends State<PickingPage> {
 
-  MapboxNavigation _directions;
-  int _run;
-  //bool _arrived = false;
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    _directions = MapboxNavigation(onRouteProgress: (arrived) async {
-
-      setState(() {
-        //_arrived = arrived;
-      });
-      if (arrived)
-        {
-          await Future.delayed(Duration(seconds: 3));
-          await _directions.finishNavigation();
-        }
-    });
-  }
+  List<PickingProduct> _products;
+  bool _progressController = true;
 
   @override
   void initState() {
-    initPlatformState();
+    getProducts();
     super.initState();
   }
 
@@ -90,6 +49,15 @@ class _PickingPageState extends State<PickingPage> {
       }
   }
 
+  void getProducts(){
+    Webservice().load(PickingProduct.all).then((products) => {
+      setState(() => {
+        _products = products,
+         _progressController = false,
+      }),
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,82 +69,110 @@ class _PickingPageState extends State<PickingPage> {
           ),
         ),
       backgroundColor: Color.fromRGBO(51, 102, 153, 1),
-        body: Column(
+        body: _products.length == 0 ?
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text("No Items Left", style: TextStyle(color: Colors.white, fontSize: 24),),
+                  RaisedButton(
+                    child: Text("Logout"),
+                    onPressed: () async {
+                      SharedPreferences preferences = await SharedPreferences.getInstance();
+                      preferences.clear();
+                     Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => LogIn()
+                          )
+                        );
+                     },
+                  )
+                ],
+              ),
+            )
+        : Column(
           children: <Widget>[
-            Center(child: Text("iofheriohfioe"),),
             Expanded(
-              child: FutureBuilder<List<PickingProduct>>(
-                future: getPick(),
-                builder: (BuildContext context, AsyncSnapshot snapshot){
-                  if(snapshot.data == null){
-                    return Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Color.fromRGBO(255, 255, 187, 1),
-                        valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(251, 202, 0, 1)),
+              child: ListView.builder(
+                itemCount: _products == null ? 0 : _products.length ,
+                itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(top: 8.0, left: 12.0, right: 12.0), 
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: Center(child: Text(_products[index].productName, style: TextStyle(fontSize: 18),),),
+                              ),
+                              Row(
+                                 mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  innerCard("Aisle", _products[index].aisle.toString()),
+                                  innerCard("Mod", _products[index].mod.toString()),
+                                  innerCard("Shelf", _products[index].shelf.toString()),
+                                  innerCard("Slot", _products[index].slot.toString())
 
+                                ],
+                              )
+                            ],
+                          ),
+                        )
                       ),
                     );
-                  }else{
-                    return Container(
-                          child: ListView.builder(
-                            itemCount: snapshot.data.length,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: EdgeInsets.only(top: 8.0, left: 12.0, right: 12.0), 
-                                child: Card(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      children: <Widget>[
-                                        Center(child: Text(snapshot.data[index].productName),),
-                                      ],
-                                    ),
-                                  )
-                                ),
-                              );
-                            },
-                          ),
-                        );
-                  }
-                },
+                }
               ),
             ),
             BottomAppBar(
               color: Color.fromRGBO(251, 202, 0, 1),
               child: Center(
-                child: RaisedButton(
-                  color: Color.fromRGBO(51, 102, 153, 1),
-                  onPressed: orderCounter == ordersCount ? _endRun : null, 
-                  child: Text("End Run"),
-                ),
-              )
+                child: IconButton(
+                  icon: Icon(Icons.camera),
+                  tooltip: "Scan",
+                  onPressed: scan,
+                )
+              ),
             ),
           ],
         ),
       );
   }
 
-  _endRun() async{
-    print("Run Finished");
-    var url = 'https://ads.morganchorlton.me/api/run/save?id=' + this._run.toString();
-    print(json.encode(compleatedOrders));
-    var response = await http.post(url, body: json.encode(compleatedOrders));
-    print(response.body);
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    preferences.clear();
-    Navigator.push(
-            context,
-            new MaterialPageRoute(
-                builder: (context) => LogIn()
-                )
-              );
-              Alert(
-      context: context,
-      type: AlertType.success,
-      title: "Trip Saved",
-      desc: "Trip Compleate",
-    ).show();
+  Widget innerCard(String type, String value){
+    return Flexible(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: <Widget>[
+            Text(type),
+            Text(value)
+            ],
+          ),
+        ),
+      )
+    );
+  }
+
+  Future scan() async {
+  String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.BARCODE);
+      print("Barcode Result:" + barcodeScanRes);
+          setState(() {
+          _products.removeWhere((item) => item.barcode == barcodeScanRes);
+          _products = _products;
+          print(_products.length);
+        });
+      
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
     }
-    
-  
+  }
 }
--- lib/models/PickingProduct.dart
+++ lib/models/PickingProduct.dart
@@ -1,8 +1,16 @@
-import 'dart:convert' show jsonDecode;
+import 'dart:convert';
+
+import 'package:ads/services/webService.dart';
+
 class PickingProduct {
+
+  int id, productID, aisle, mod, shelf;
+  String productName, slot, barcode;
+
   PickingProduct({
     this.id,
     this.productName,
+    this.barcode,
     //Product Location
     this.aisle,
     this.mod,
@@ -10,26 +18,39 @@ class PickingProduct {
     this.slot
   });
 
-  int id, productID, aisle, mod, shelf;
-  String productName, slot;
-
-
   factory PickingProduct.fromJson(Map<String, dynamic> json) {
-    return new PickingProduct(
+    return PickingProduct(
       id: json['id'],
       productName: json['products']['name'],
+      barcode: json['products']['barcode'],
       aisle: json['products']['product_location']['aisle'],
       mod: json['products']['product_location']['mod'],
       shelf: json['products']['product_location']['shelf'],
       slot: json['products']['product_location']['slot'],
     );
   }
+
   Map<String, dynamic> toJson() => {
     'id': id,
     'name': productName,
+    'barcode' : barcode,
     'aisle': aisle,
     'mod': mod,
     'shelf': shelf,
     'slot': slot
   };
+
+  static Resource<List<PickingProduct>> get all {
+    
+    return Resource(
+      url: "https://ads.morganchorlton.me/api/picking/get",
+      parse: (response) {
+        final result = json.decode(response.body); 
+        Iterable list = result;
+        return list.map((model) => PickingProduct.fromJson(model)).toList();
+      }
+    );
+
+  }
+
 }
